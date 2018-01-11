9e68c4e8336e917bc8064a7a30b4c19434cab477
Andrzej Marek <andrzej.kx@gmail.com>
2018-01-11 21:22:27 +0100

    03 extract

--- src/index.js
+++ src/index.js
@@ -4,18 +4,21 @@ import ReactDOM from 'react-dom';
 import events from './data/events.json';
 
 ReactDOM.render(
-    <ul>
-        {events.map((item) => {
-            const date = new Date(item.date);
-            if(date >= Date.now()){
-                return (
-                    <li key={item.id}>
-                        <strong>{item.name}</strong><br />
-                        Miejsce: {item.place}<br />
-                        Data: {item.date} Godzina: {item.time}
-                    </li>
-                );
-            }
-            return null;
-        })}
-    </ul>, document.getElementById('root'));
+  <ul>
+    {events.map(item => {
+      const date = new Date(item.date);
+
+      if (date >= Date.now()) {
+        return (
+          <li key={item.id}>
+            <strong>{item.name}</strong><br />
+            Gdzie: {item.place}<br />
+            Kiedy: {item.date} - {item.time}
+          </li>
+        );
+      }
+
+      return null;
+    })}
+  </ul>
+  , document.getElementById('root'));
