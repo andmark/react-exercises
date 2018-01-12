7dcc86b3270d7efc6479695a060fc9116ae64385
Andrzej Marek <andrzej.kx@gmail.com>
2018-01-12 21:48:37 +0100

    Add events.js file

--- src/index.js
+++ src/index.js
@@ -1,28 +1,6 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 
 import events from './data/events.json';
-
-const getEvents = () => {
-return (
-  <ul>
-  {events.map(item => {
-    const date = new Date(item.date);
-
-    if (date >= Date.now()) {
-      return (
-        <li key={item.id}>
-          <strong>{item.name}</strong><br />
-          Gdzie: {item.place}<br />
-          Dnia: {item.date}, Godzina: {item.time}
-        </li>
-      );
-    }
-
-    return null;
-  })}
-</ul>
-)
-}
-
-ReactDOM.render(getEvents(), document.getElementById('root'));
+// import getEvents from './events.js' // default
+import { getEvents } from './events.js'
+ReactDOM.render(getEvents(events), document.getElementById('root'));
