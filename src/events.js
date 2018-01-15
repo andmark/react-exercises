4fbf6a73ee85c99b9c7264bc6e30bb19e65b334a
Andrzej Marek <andrzej.kx@gmail.com>
2018-01-15 14:41:04 +0100

    Prop types

--- src/events.js
+++ src/events.js
@@ -1,25 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Events extends React.Component {
+    static propTypes = {
+        happening: PropTypes.array.isRequired
+    };
     render() {
-    return (
-        <ul>
-            {this.props.happening.map(item => {
-            const date = new Date(item.date);
-        
-            if (date >= Date.now()) {
-                return (
-                <li key={item.id}>
-                    <strong>{item.name}</strong><br />
-                    Gdzie: {item.place}<br />
-                    Dnia: {item.date}, Godzina: {item.time}
-                </li>
-                );
-            }
-            return null;
-            })}
-        </ul>
-      );
+        return (
+            <ul>
+                {this.props.happening.map(item => {
+                    const date = new Date(item.date);
+                    if (date >= Date.now()) {
+                        return (
+                        <li key={item.id}>
+                            <strong>{item.name}</strong><br />
+                            Gdzie: {item.place}<br />
+                            Dnia: {item.date}, Godzina: {item.time}
+                        </li>
+                        );
+                    }
+                    return null;
+                })}
+            </ul>
+        );
     }
 };
 
